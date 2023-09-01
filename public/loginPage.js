4427ad2870410e10a3065f9b46fb5139db7287fd
MarkDolgin <33152312+MarkDolgin@users.noreply.github.com>
2023-09-01 06:10:09 +0300

    loginPage.js

---
+++ public/loginPage.js
@@ -0,0 +1,25 @@
+//Строгий режим
+"use strict";
+
+//Создаем объект
+const userForm = new UserForm();
+
+//Функция для входа в личный кабинет
+userForm.loginFormCallback = (data) =>
+  ApiConnector.login(data, (response) => {
+    if (response.success) {
+      location.reload();
+    } else {
+      userForm.setLoginErrorMessage(response.error);
+    }
+  });
+
+//Фукнция для регистрации
+userForm.registerFormCallBack = (data) =>
+  ApiConnector.register(data, (response) => {
+    if (response.success) {
+      location.reload();
+    } else {
+      userForm.setRegisterErrorMessage(response.error);
+    }
+  });
